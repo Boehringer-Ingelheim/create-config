22a90ff31ad85daecd5871eb3abb7fd46c978cd6
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2025-09-15 10:19:36 +0200

    feat: add eslint+prettier option

--- files/__eslint.config.mjs
+++ files/__eslint.config.mjs
@@ -1,6 +1,12 @@
 import boehringer from '@boehringer-ingelheim/eslint-config';
+import prettier from 'eslint-plugin-prettier/recommended';
 import { defineConfig } from 'eslint/config';
 
+// See https://github.com/Boehringer-Ingelheim/eslint-config/blob/master/README.md
 export default defineConfig(
-  boehringer.configs.strict
-)
+  boehringer.configs.strict,
+  // Should be second to last
+  prettier,
+  // Should be last
+  boehringer.configs.prettierDisable,
+);
--- src/packages.list.ts
+++ src/packages.list.ts
@@ -3,4 +3,14 @@ import type { ConfigPackage } from './files.list.types';
 export const packageFileDict: ConfigPackage[] = [
   { formName: 'prettier', packages: ['prettier', '@boehringer-ingelheim/prettier-config'] },
   { formName: 'eslint', packages: ['eslint', '@boehringer-ingelheim/eslint-config'] },
+  {
+    formName: 'eslint + prettier',
+    packages: [
+      'eslint',
+      '@boehringer-ingelheim/eslint-config',
+      'prettier',
+      '@boehringer-ingelheim/prettier-config',
+      'eslint-plugin-prettier',
+    ],
+  },
 ];
